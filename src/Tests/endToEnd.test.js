70d8f040150eea26869732d46c8f3a9144bc95ef
Louis Doan <louisdoan9@gmail.com>
2024-05-28 17:39:28 -0400

    Setup Selenium/Jest for end to end testing

---
+++ src/Tests/endToEnd.test.js
@@ -0,0 +1,8 @@
+const { By, Builder, Browser, until } = require('selenium-webdriver');
+
+describe('related tests block', () => {
+	beforeAll(() => {});
+	afterAll(() => {});
+
+	test('sample test', () => {});
+});
